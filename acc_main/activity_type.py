e48d12d18fcf575bc54862c6727b975310e455e2
Ruben Hias <ruben.hias@gmail.com>
2020-08-08 18:41:13 +0200

    Made cost equal to purchase price

--- acc_main/activity_type.py
+++ acc_main/activity_type.py
@@ -7,3 +7,15 @@ class ActivityType(models.Model):
 
     name = fields.Char("Activity Name", required=True)
     code = fields.Char("Activity code", required=True)
+
+
+class ProductInherit(models.Model):
+    _inherit = "product.product"
+    _name = "product.product"
+
+    standard_price = fields.Monetary(compute="_compute_cost")
+
+    def _compute_cost(self):
+        for record in self:
+            if record.seller_ids:
+                record.standard_price = record.seller_ids[0].price
