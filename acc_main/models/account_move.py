717b7977142ad15f4bc5f8f0b4f2904d6c79d3ab
rubenh-be <ruben.hias@gmail.com>
2020-09-09 11:56:06 +0000

    Have SO delivery address on bill

--- acc_main/models/account_move.py
+++ acc_main/models/account_move.py
@@ -6,6 +6,18 @@ class AccountMoveInherit(models.Model):
     so_invoice_id = fields.Many2one('res.partner', compute='_get_so_invoice_id')
 
     @api.model
+    def create(self, vals):
+        print(vals['invoice_origin'])
+        so = self.env['sale.order'].search([('name', '=', vals['invoice_origin'])])
+        if so:
+            vals['partner_shipping_id'] = so.partner_shipping_id
+        else:
+            po = self.env['purchase.order'].search([('name', '=', vals['invoice_origin'])])
+            vals['partner_shipping_id'] = po.dest_address_id.id
+        print(vals['partner_shipping_id'])
+        return super(AccountMoveInherit, self).create(vals)
+
+    @api.model
     @api.depends('invoice_origin')
     def _get_so_invoice_id(self):
         for rec in self:
