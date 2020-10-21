e180d74a28a888d21d865366274a02a9a731792c
rubenh-be <ruben.hias@gmail.com>
2020-10-21 12:33:49 +0000

    Added serial number history log

---
+++ serial_entry/models/stock_production_lot.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class StockProductionLot(models.Model):
+    _inherit = "stock.production.lot"
+
+    log_entries = fields.One2many("log.entry", "serial_number_ref")
+    delivery_address = fields.Many2one("res.partner", related="purchase_order_ids.dest_address_id")
