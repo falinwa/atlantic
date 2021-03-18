aaab8552dc65a522fc9bb52995c7c8ead71347ee
Ruben Hias <ruben.hias@gmail.com>
2021-03-18 20:25:13 +0100

    Added generic product module.

---
+++ generic_product/models/product.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api, _
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    fal_stock_product = fields.Boolean(
+        string='Is Stock Product',
+        default=False)
+    property_account_income_id = fields.Many2one(copy=True)
+    property_account_expense_id = fields.Many2one(copy=True)
+    is_generic_product = fields.Boolean(string='Generic Product', default=False)
