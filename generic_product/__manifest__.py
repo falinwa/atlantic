46c65e9bd9b9144017cab6bb276bc3800128b668
Ruben Hias <ruben.hias@gmail.com>
2021-03-18 20:25:13 +0100

    Added generic product module.

---
+++ generic_product/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Product Enhancement',
+    'version': '14.1.0.0',
+    'category': 'Product',
+    'description': '''
+        Added Generic products
+    ''',
+    'depends': [
+        'inter_company_rules',
+    ],
+    'data': [
+        'views/fal_product_views.xml',
+    ],
+    'images': [
+    ],
+    'demo': [
+    ],
+    'price': 360.00,
+    'currency': 'EUR',
+    'application': False,
+}
