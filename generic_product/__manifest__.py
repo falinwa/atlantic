e05b50a2aea8998f83d3b90aeeb05fea5812b130
Ruben Hias <ruben.hias@gmail.com>
2021-03-19 09:11:07 +0100

    Fixed typo

--- generic_product/__manifest__.py
+++ generic_product/__manifest__.py
@@ -9,13 +9,6 @@
         'inter_company_rules',
     ],
     'data': [
-        'views/fal_product_views.xml',
+        'views/product_views.xml',
     ],
-    'images': [
-    ],
-    'demo': [
-    ],
-    'price': 360.00,
-    'currency': 'EUR',
-    'application': False,
 }
