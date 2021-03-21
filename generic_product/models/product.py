f6c7bb860b9879b78cd899b95e6747ca4b530cf6
Ruben Hias <ruben.hias@gmail.com>
2021-03-21 15:19:30 +0100

    Refactoring generic

--- generic_product/models/product.py
+++ generic_product/models/product.py
@@ -5,8 +5,9 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     stock_product = fields.Boolean(
-        string='Is Stock Product',
+        string='Stockable?',
         default=False)
+
+    is_generic_product = fields.Boolean(string='Generic?', default=False)
     property_account_income_id = fields.Many2one(copy=True)
     property_account_expense_id = fields.Many2one(copy=True)
-    is_generic_product = fields.Boolean(string='Generic Product', default=False)
